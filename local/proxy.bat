5af4d0dd5bb2f8fd69e8331a7d8bbe26b74b155d
Phus Lu <phuslu@hotmail.com>
2014-06-07 15:39:38 +0800

    refine proxy.bat

--- local/proxy.bat
+++ local/proxy.bat
@@ -1,4 +1,4 @@
 rem @set GEVENT_LOOP=uvent.loop.UVLoop
-@set GEVENT_RESOLVER=gevent.resolver_thread.Resolver
+@set GEVENT_RESOLVER=block
 @set GOAGENT_LISTEN_VISIBLE=1
 @start "GoAgent" "%~dp0python27.exe" "%~dp0..\local\proxy.py"
